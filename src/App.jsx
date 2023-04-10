e56f3625ce18963412e330080577234907066d33
najmusskb <najmussakib173@gmail.com>
2023-04-10 13:33:00 +0600

    change routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/home",
+        path: "/",
         element: <Home></Home>,
       },
       {
